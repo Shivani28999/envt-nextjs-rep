355d74628286352c18ff230cddf1cf3043ace155
Shivani28999 <lollshiv@amazon.com>
2026-01-13 11:28:20 +0530

    Tenth commit

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,6 @@ priority
 To get started, edit the page.tsx file.
 </h1>
 <h1>
-<div>Accesskeyid{process.env.AMPLIFY_AWS_ACCESS_KEY_ID}</div>
 <div>{temp}</div>
 </h1>
 <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
